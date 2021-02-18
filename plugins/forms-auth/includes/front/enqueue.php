1dd2efdfcb758d506f46e6e46be91a68d8906ca7
deyems <adeyemisola11@gmail.com>
2021-02-18 12:16:37 +0100

    assets are enqueued

---
+++ plugins/forms-auth/includes/front/enqueue.php
@@ -0,0 +1,28 @@
+<?php
+
+function db_enqueue_scripts(){
+    
+    wp_register_style('db_bootstrap', 
+    plugins_url('/assets/css/bootstrap.css', AUTH_FORM_PLUGIN_URL));
+    wp_register_style('db_font_awesome', 
+    plugins_url('/assets/css/font-awesome.css', AUTH_FORM_PLUGIN_URL));
+    wp_register_style('db_custom', 
+    plugins_url('/assets/css/mycss.css', AUTH_FORM_PLUGIN_URL));
+    
+    wp_enqueue_style('db_bootstrap');
+    wp_enqueue_style('db_font_awesome');
+    wp_enqueue_style('db_custom');
+    
+    wp_register_script('db_main', 
+    plugins_url('/assets/js/main.js', AUTH_FORM_PLUGIN_URL),
+    ['jquery'],
+    '1.0.0',
+    true);
+    
+    wp_localize_script('db_main', 'db_recipe_obj', [
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'home_url' => home_url('/')
+    ]);
+
+    wp_enqueue_script('db_main');
+}
